f938e2298619826cbe0ced61d14d2529bbfbc89c
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-26 21:58:02 +0530

    Working on POST Operation

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const tasks = require("./MOCK_DATA.json");
+const path = require("path");
 const fs = require("fs");
 
 const app = express();
+
+const filePath = path.join(__dirname, "MOCK_DATA.json");
 
 app.get("/", (req, res) => {
     return res.send("Hi, This is Home Page of Task Manager");
@@ -15,7 +18,17 @@ app.get("/tasks", (req, res) => {
         </ul>
     `
     return res.send(html);
-})
+});
+
+app.post("/tasks", (req, res) => {
+    const newTask = req.body.task;
+
+    tasks.push({ id: tasks.length + 1, task: newTask });
+
+    fs.appendFile(filePath,)
+
+    return res.send("Task added successfully");
+});
 
 app.listen(7000, () => { console.log("Server Started") });
 
