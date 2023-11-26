9ad34628bd6d701c2f68111ea94245e42bf07df7
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-26 19:17:29 +0530

    GET Operation Done

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const tasks = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.send("Hi, This is Home Page");
+    return res.send("Hi, This is Home Page of Task Manager");
 });
 
+app.get("/tasks", (req, res) => {
+    const html = `
+        <ul>
+            ${tasks.map((task) => `<li>${task.task}</li>`).join(`<br>`)}
+        </ul>
+    `
+    return res.send(html);
+})
+
 app.listen(7000, () => { console.log("Server Started") });
+
