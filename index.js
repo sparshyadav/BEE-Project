4715fc9fba3744b4314b189a6edfcd1a658433c5
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-26 17:54:07 +0530

    Created Mock Data File

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    return res.send("Hi, This is Home Page");
+});
+
 app.listen(7000, () => { console.log("Server Started") });
