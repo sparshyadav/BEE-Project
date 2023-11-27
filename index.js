e1418ae5638c84a612ab5798ab4417dfc8f0b323
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2023-11-27 22:12:39 +0530

    PATCH Request Formatted and Updated

--- index.js
+++ index.js
@@ -11,39 +11,39 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.send("Hi, This is Home Page of Task Manager");
+    return res.send("Hi, This is Home Page of Task Manager");
 });
 
 app.get("/tasks", (req, res) => {
-  const html = `
+    const html = `
         <ul>
             ${tasks.map((task) => `<li>${task.task}</li>`).join(`<br>`)}
         </ul>
     `;
-  return res.send(html);
+    return res.send(html);
 });
 
 app.post("/tasks", (req, res) => {
-  const newTask = { task: req.body.task };
+    const newTask = { task: req.body.task };
 
-  tasks.push({ id: tasks.length + 1, ...newTask });
+    tasks.push({ id: tasks.length + 1, ...newTask });
 
-  fs.writeFile(filePath, JSON.stringify(tasks), (err, data) => {
-    return res.json({ status: "Pending" });
-  });
+    fs.writeFile(filePath, JSON.stringify(tasks), (err, data) => {
+        return res.json({ status: "Pending" });
+    });
 });
 
-app.patch("/task/:id", (req, res) => {
-  var index = req.params.id;
-  var newTask = req.body.task;
+app.patch("/tasks/:id", (req, res) => {
+    var index = req.params.id - 1;
+    var newTask = req.body.task;
 
-  data[index].task = newTask;
+    tasks[index].task = newTask;
 
-  fs.writeFile(filePath, JSON.stringify(tasks), (err, data) => {
-    res.send({ sucess: true });
-  });
+    fs.writeFile(filePath, JSON.stringify(tasks), (err, data) => {
+        res.send({ sucess: "Content Changed" });
+    });
 });
 
 app.listen(7000, () => {
-  console.log("Server Started");
+    console.log("Server Started");
 });
